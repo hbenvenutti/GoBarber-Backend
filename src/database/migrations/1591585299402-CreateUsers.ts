9cc617e7ffd4c64d1892c90d1ecc00585242dc43
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-06-09 23:34:39 -0300

    Aula: TypeORM Model User

--- src/database/migrations/1591585299402-CreateUsers.ts
+++ src/database/migrations/1591585299402-CreateUsers.ts
@@ -1,28 +1,31 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointment1591054933431
-  implements MigrationInterface {
+export default class CreateUsers1591585299402 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
-            type: 'varchar', // Varchar because it is an uuid;
+            type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone', // timestamp if not using postgres
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +43,6 @@ export default class CreateAppointment1591054933431
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
